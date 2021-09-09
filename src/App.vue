2c1f9d76b72c77d733eeeb7f342f6d110afee8f8
Achilles-one <2276635831@qq.com>
2021-09-09 20:51:34 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to=".testmo" v-if="showModal">
+    <modal theme="sale" @close="toggleModal">
+      <!-- @close="toggleModal"表示父组件监听到事件后执行函数 -->
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>使用slot将父组件的内容放到子组件内</h1>
+      <p>这个也包括</p>
+    </modal>
+    
+  </teleport>
+
+  <div v-if="showModal2">
+    <modal theme="bl" @close="toggleModal2">
+      <!-- @close="toggleModal"表示父组件监听到事件后执行函数 -->
+      <!-- <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template> -->
+      <h1>第二个modal</h1>
+      <p>这个也包括</p>
+    </modal>
+  </div>
+
+  <button @click.shift="toggleModal">open modal (shift)</button>
+  <br>
+  <button @click="toggleModal2">show modal2</button>
+
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import modal from "./components/modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    modal,
+  },
+  data() {
+    return {
+      title: "My VUE :)",
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app, .testmo {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/modal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+
+    <div class="modal" :class="{sale: theme === 'sale' }">
+        <!-- <h1>hello</h1> -->
+        <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  
+    <div class="modal" :class="{ bl: theme === 'bl' }">
+      <slot></slot>
+    </div>
+  
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["theme",],
+  methods: {
+    closeModal() {
+      // 发射事件，使父组件监听
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+
+<style>
+.modal.sale {
+  background: orangered;
+  /* color: white; */
+}
+.modal.bl {
+    background: turquoise;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 50px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+.modal .actions {
+  text-align: center;
+  margin: 20px 0 10px 0;
+}
+.modal .actions a {
+  color: rgb(19, 18, 18);
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+</style>
